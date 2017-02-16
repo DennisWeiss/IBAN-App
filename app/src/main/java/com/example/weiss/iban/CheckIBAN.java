fe22dcd501eb01506165844aa0dfa0b5aaad0d89
DennisWeiss <weiss.dennis98@yahoo.de>
2017-02-16 15:28:05 +0100

    worked on CheckIBAN Activity

--- app/src/main/java/com/example/weiss/iban/CheckIBAN.java
+++ app/src/main/java/com/example/weiss/iban/CheckIBAN.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.media.Image;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBar;
@@ -12,8 +13,11 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Spinner;
 
 /**
@@ -27,5 +31,24 @@ public class CheckIBAN extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.checkiban);
 
+        final EditText iban = (EditText) findViewById(R.id.editText);
+        Button button = (Button) findViewById(R.id.button);
+        final ImageView image = (ImageView) findViewById(R.id.imageView);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isValid(iban.getText().toString())) {
+                    image.setImageResource(R.drawable.tick);
+                } else {
+                    image.setImageResource(R.drawable.cross);
+                }
+            }
+        });
+    }
+
+    boolean isValid(String iban) {
+        //TODO: Implementation for Checking IBAN
+        return true;
     }
 }
